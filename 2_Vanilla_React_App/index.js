b32d5121e2b1da0a223578d9e82e97101ddd045a
Yatin_Chaudhari <c.yatin727@gmail.com>
2023-11-18 19:27:53 +0530

    CRA-Vite-VanillaReact

--- 2_Vanilla_React_App/index.js
+++ 2_Vanilla_React_App/index.js
@@ -106,3 +106,39 @@ const App = () => {
 };
 
 ReactDOM.createRoot(rootElement).render(<App />);
+
+/*
+   ? Create React App using Create-React-App.
+   * npx :
+   - npx create-react-app my-app
+   * npm :
+   - npm init react-app my-app
+   * Yarn:
+   - yarn create react-app my-
+   
+   - Decide which package manager to use -Yarn or Npm .
+
+   * To Start App :
+   npm start or yarn start
+   * index.js
+      - if Jsx is present .jsx extension or .js extension dosent matter.
+
+   
+ */
+
+/*
+   ? Create React App using Vite.
+
+   * npx :
+   - npm create vite@latest
+
+   * Yarn:
+   - yarn create vite
+   
+   - Decide which package manager to use  -Yarn or Npm .
+
+   * To Start App :
+   npm run dev or yarn start
+   * main.jsx
+   - if Jsx is present we need to use .jsx extension.
+ */
