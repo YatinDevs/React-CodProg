2e734f9e6eb724505455bdd89e10cd6ad55767f8
YatinDevs <c.yatin727@gmail.com>
2023-12-01 17:17:13 +0530

    use-Reducer-todo-app

--- todo-app-usingreducer/src/App.jsx
+++ todo-app-usingreducer/src/App.jsx
@@ -2,12 +2,18 @@ import { useState, useReducer } from "react";
 import "./App.css";
 import AddTodoForm from "./components/AddTodoForm";
 import DisplayTodos from "./components/DisplayTodos";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function reducer(todos, action) {
   console.log("inside reducer function");
   switch (action.type) {
-    case "ADD":
+    case "ADDTODO":
+      console.log("added", action, action.payload.id);
+
+      return [...todos, action.payload.newTodo];
       break;
+
     case "DELETE":
       console.log("deleted", action, action.payload.id);
 
@@ -42,8 +48,9 @@ function App() {
 
   return (
     <div className="container">
+      <ToastContainer />
       <h1 className="main-title">Todo List</h1>
-      <AddTodoForm />
+      <AddTodoForm dispatch={dispatch} />
       <DisplayTodos todos={todos} dispatch={dispatch} />
     </div>
   );
--- todo-app-usingreducer/src/components/AddTodoForm.jsx
+++ todo-app-usingreducer/src/components/AddTodoForm.jsx
@@ -1,13 +1,40 @@
 import React from "react";
+import { toast } from "react-toastify";
+import { useState } from "react";
+function AddTodoForm({ dispatch }) {
+  const [title, setTitle] = useState("");
 
-function AddTodoForm() {
-  const AddTodo = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("form Submitted.");
+    if (title.trim().length === 0) {
+      toast.error("Please Fill Input", { autoClose: 2000 });
+      return;
+    }
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title: title,
+      completed: false,
+    };
+
+    dispatch({
+      type: "ADDTODO",
+      payload: { newTodo: newTodo },
+    });
+    setTitle("");
+  };
   return (
     <div>
-      <form className="todoForm">
-        <input className="todoForm_input" type="text" />
+      <form className="todoForm" onSubmit={handleSubmit}>
+        <input
+          className="todoForm_input"
+          type="text"
+          value={title}
+          placeholder="Add Todo"
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <button type="submit" className="todoForm_Btn">
-          Add
+          Add Todo
         </button>
       </form>
     </div>
